138a797328fb93fbedb1c68e4a9f8965bf054830
Apoorv Goyal <apoorvelous@gmail.com>
2022-12-22 14:27:23 +0530

    home section 1 added

--- app/views/views.py
+++ app/views/views.py
@@ -1,8 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__, url_prefix="/")
 
 @views.route("/")
 def home():
-    return "<h1 style='margin-top: 20px; display: flex; justify-content: center;'>Virtual Tech School in Under Construction!</h1>"
-
+    return render_template(
+        "home/home.html"
+    )
