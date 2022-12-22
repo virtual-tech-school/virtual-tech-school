e468ee19425ef3f90c2549be7076ef2ae7c1afa4
Apoorv Goyal <apoorvelous@gmail.com>
2022-12-23 02:33:20 +0530

    counter added

--- app/views/views.py
+++ app/views/views.py
@@ -4,6 +4,33 @@ views = Blueprint('views', __name__, url_prefix="/")
 
 @views.route("/")
 def home():
+    stats_list = [
+        {
+            "name": "YouTube",
+            "icon": "youtube.svg",
+            "link": "https://www.youtube.com/c/ApoorvGoyalMain",
+            "value": 4950
+        },
+        {
+            "name": "Discord",
+            "icon": "discord.svg",
+            "link": "https://discord.com/invite/EYB8tQxjxH",
+            "value": 1500
+        },
+        {
+            "name": "Twitter",
+            "icon": "twitter.svg",
+            "link": "https://twitter.com/virtechschool",
+            "value": 250
+        },
+        {
+            "name": "Hashnode",
+            "icon": "hashnode.svg",
+            "link": "https://virtualtechschool.hashnode.dev/",
+            "value": 70
+        }
+    ]
     return render_template(
-        "home/home.html"
+        "home/home.html",
+        stats=stats_list
     )
